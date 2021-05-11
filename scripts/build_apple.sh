119899f24c534cdb916d8f8cd6a507bd467de95c
Pasin Suriyentrakorn <pasin@couchbase.com>
2021-05-11 14:53:52 -0700

    Add a script for building xcframework
    * Added scripts/build_apple.sh for building xcframework. Use —ee option to build ee version.
    * Fixed mergeIntoStaticLib.sh for the file paths including space.
    * Made C++ headers project level depency so they will not be copied into the framework file.
    * Fixed CouchbaseLite.modulemap and Removed C++ headers.
    * Removed Fleece’s C++ headers files in Copy Fleece Header build pharse.

---
+++ scripts/build_apple.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+function usage
+{
+  echo "Usage: ${0} [--ee]"
+}
+
+while [[ $# -gt 0 ]]
+do
+  key=${1}
+  case $key in
+    --ee)
+    EE="Y"
+    ;;
+    *)
+    usage
+    exit 3
+    ;;
+  esac
+  shift
+done
+
+if [ -z "$EE" ]
+then
+  echo "Build for Community Edition ..."
+  CONFIGURATION="Release"
+else
+  echo "Build for Enterprise Edition ..."
+  CONFIGURATION="Release_EE"
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+pushd $SCRIPT_DIR/.. > /dev/null
+
+# Use absolute path to workaround an xcodebuild bug that cannot recognize the dSym files 
+# using their relative paths when creating the xcframework file.
+OUTPUT_DIR=`pwd`/build_apple_out
+BUILD_DIR="$OUTPUT_DIR/build"
+mkdir -p "$BUILD_DIR"
+
+FRAMEWORK_NAME="CouchbaseLite"
+
+# this will be used to collect all destination framework path with `-framework`
+# to include them in `-create-xcframework`
+FRAMEWORK_PATH_ARGS=()
+
+# arg1 = target destination for which the archive is built for. E.g., "generic/platform=iOS"
+function xcarchive
+{
+  PLATFORM_NAME=${1}
+  DESTINATION=${2}
+  
+  echo "Archiving for ${DESTINATION}..."
+  ARCHIVE_PATH=${BUILD_DIR}/${PLATFORM_NAME}
+  xcodebuild archive \
+    -scheme "CBL_C Framework" \
+    -configuration "${CONFIGURATION}" \
+    -destination "${DESTINATION}" \
+    ${BUILD_VERSION} ${BUILD_NUMBER} \
+    -archivePath "${ARCHIVE_PATH}/${FRAMEWORK_NAME}.xcarchive" \
+    "ONLY_ACTIVE_ARCH=NO" "BITCODE_GENERATION_MODE=bitcode" \
+    "CODE_SIGNING_REQUIRED=NO" "CODE_SIGN_IDENTITY=" \
+    "SKIP_INSTALL=NO"
+  
+  FRAMEWORK_PATH_ARGS+=("-framework "${ARCHIVE_PATH}/${FRAMEWORK_NAME}.xcarchive/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework" \
+    -debug-symbols "${ARCHIVE_PATH}/${FRAMEWORK_NAME}.xcarchive/dSYMs/${FRAMEWORK_NAME}.framework.dSYM"")
+  echo "Finished archiving ${DESTINATION}."
+}
+
+# Build and archive binary for each platform:
+xcarchive "iossim" "generic/platform=iOS Simulator"
+xcarchive "ios" "generic/platform=iOS"
+xcarchive "macos" "generic/platform=macOS"
+
+# Create xcframework
+echo "Creating XCFramework...: ${FRAMEWORK_PATH_ARGS}"
+xcodebuild -create-xcframework \
+    -output "${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework" \
+    ${FRAMEWORK_PATH_ARGS[*]}
+
+# Remove build directory
+rm -rf ${BUILD_DIR}
+echo "Finished creating XCFramework. Output at "${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework""
+popd  > /dev/null
