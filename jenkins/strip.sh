aa609baa49946962ce24689db482defe75153c68
Jim Borden <jim.borden@couchbase.com>
2021-05-28 23:55:44 +0000

    Build server script for mac/Linux (#128)

---
+++ jenkins/strip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PREFIX=""
+WORKING_DIR="${1}"
+
+if [[ $# > 1 ]]; then
+  PREFIX="${2}"
+fi
+
+pushd $WORKING_DIR
+COMMAND="find . -name \"*.a\" | xargs ${PREFIX}strip --strip-unneeded"
+eval ${COMMAND}
+rm libLiteCore.so
+make -j8 LiteCore
+COMMAND="${PREFIX}objcopy --only-keep-debug libCouchbaseLiteC.so libCouchbaseLiteC.so.sym"
+eval ${COMMAND}
+COMMAND="${PREFIX}strip --strip-unneeded libCouchbaseLiteC.so"
+eval ${COMMAND}
+COMMAND="${PREFIX}objcopy --add-gnu-debuglink=libCouchbaseLiteC.so.sym libCouchbaseLiteC.so"
+eval ${COMMAND}
+popd
