42457462a94c18872fbe680e77f27e4b1007c97c
Pasin Suriyentrakorn <pasin@couchbase.com>
2021-07-06 16:48:22 -0700

    Add EE option

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,11 +1,38 @@
 #!/bin/bash -e
 
+function usage 
+{
+  echo "Usage: ${0} [--EE]" 
+}
+
+while [[ $# -gt 0 ]]
+do
+  key=${1}
+  case $key in
+      --EE)
+      EE="Y"
+      ;;
+      *)
+      usage
+      exit 3
+      ;;
+  esac
+  shift
+done
+
+if [ -z "$EE" ]
+then
+  BUILD_ENTERPRISE="OFF"
+else
+  BUILD_ENTERPRISE="ON"
+fi
+
 SCRIPT_DIR=`dirname $0`
 pushd $SCRIPT_DIR/.. > /dev/null
 
 mkdir -p build_cmake
 pushd build_cmake > /dev/null
-cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE_ENABLED=ON ..    
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_ENTERPRISE=${BUILD_ENTERPRISE} -DCODE_COVERAGE_ENABLED=ON ..    
 core_count=`getconf _NPROCESSORS_ONLN`
 make -j `expr $core_count + 1`
 
