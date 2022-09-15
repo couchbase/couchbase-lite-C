1eea87580cc9655f4946fcddc13e1f57cd772d99
Pasin Suriyentrakorn <pasin@couchbase.com>
2022-09-15 18:33:24 +0700

    Add API Doc in C++ Document.hh (#352)

--- include/cbl++/Document.hh
+++ include/cbl++/Document.hh
@@ -31,28 +31,43 @@ CBL_ASSUME_NONNULL_BEGIN
 namespace cbl {
     class MutableDocument;
 
+    /** Immutable Document. */
     class Document : protected RefCounted {
     public:
         // Metadata:
 
+        /** A document's ID */
         std::string id() const                     {return asString(CBLDocument_ID(ref()));}
 
+        /** A document's revision ID, which is a short opaque string that's guaranteed to be unique to every change made to
+            the document. If the document doesn't exist yet, this function returns an empty string.  */
         std::string revisionID() const             {return asString(CBLDocument_RevisionID(ref()));}
 
+        /** A document's current sequence in the local database.
+            This number increases every time the document is saved, and a more recently saved document
+            will have a greater sequence number than one saved earlier, so sequences may be used as an
+            abstract 'clock' to tell relative modification times. */
         uint64_t sequence() const                  {return CBLDocument_Sequence(ref());}
         
+        /** A document's collection or NULL for the new document that hasn't been saved. */
         Collection collection() const              {return Collection(CBLDocument_Collection(ref()));}
 
         // Properties:
 
+        /** A document's properties as an immutable dictionary. */
         fleece::Dict properties() const            {return CBLDocument_Properties(ref());}
 
+        /** A document's properties as JSON. */
         alloc_slice propertiesAsJSON() const       {return alloc_slice(CBLDocument_CreateJSON(ref()));}
 
+        /** A subscript operator to access a document's property value by key. */
         fleece::Value operator[] (slice key) const {return properties()[key];}
 
         // Operations:
-
+        
+        /** Creates a new mutable Document instance that refers to the same document as the original.
+            If the original document has unsaved changes, the new one will also start out with the same
+            changes; but mutating one document thereafter will not affect the other. */
         inline MutableDocument mutableCopy() const;
 
     protected:
@@ -83,29 +98,59 @@ namespace cbl {
     };
 
 
+    /** Mutable Document. */
     class MutableDocument : public Document {
     public:
+        /** Creates a new, empty document in memory, with a randomly-generated unique ID.
+            It will not be added to a database until saved. */
         explicit MutableDocument(nullptr_t)             {_ref = (CBLRefCounted*)CBLDocument_CreateWithID(fleece::nullslice);}
+        
+        /** Creates a new, empty document in memory, with the given ID.
+            It will not be added to a database until saved.
+            @note If the given ID conflicts with a document already in the database, that will not
+                  be apparent until this document is saved. At that time, the result depends on the
+                  conflict handling mode used when saving; see the save functions for details.
+            @param docID  The ID of the new document, or NULL to assign a new unique ID. */
         explicit MutableDocument(slice docID)           {_ref = (CBLRefCounted*)CBLDocument_CreateWithID(docID);}
 
+        /** Returns a mutable document's properties as a mutable dictionary.
+            You may modify this dictionary and then call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes.
+            @note  When accessing nested collections inside the properties as a mutable collection
+                   for modification, use \ref MutableDict::getMutableDict() or \ref MutableDict::getMutableArray() */
         fleece::MutableDict properties()                {return CBLDocument_MutableProperties(ref());}
 
+        /** Sets a property key and value.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes. */
         template <typename V>
         void set(slice key, const V &val)               {properties().set(key, val);}
+        
+        /** Sets a property key and value.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes. */
         template <typename K, typename V>
         void set(const K &key, const V &val)            {properties().set(key, val);}
 
+        /** A subscript operator to access a document's property value by key for either getting or setting the value.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes. */
         fleece::keyref<fleece::MutableDict,fleece::slice> operator[] (slice key)
                                                         {return properties()[key];}
 
+        /** Sets a mutable document's properties.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes.
+            @param properties  The document properties. */
         void setProperties(fleece::MutableDict properties) {
             CBLDocument_SetProperties(ref(), properties);
         }
 
+        /** Sets a mutable document's properties.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes.
+            @param properties  The document properties. */
         void setProperties(fleece::Dict properties) {
             CBLDocument_SetProperties(ref(), properties.mutableCopy());
         }
 
+        /** Sets a mutable document's properties from a JSON Dictionary string.
+            Call \ref Collection::saveDocument(MutableDocument &doc) to persist the changes.
+            @param json  A JSON Dictionaryt string */
         void setPropertiesAsJSON(slice json) {
             CBLError error;
             if (!CBLDocument_SetJSON(ref(), json, &error))
@@ -196,11 +241,9 @@ namespace cbl {
         return MutableDocument::adopt(CBLDatabase_GetMutableDocument(ref(), id, &error), &error);
     }
 
-
     inline void Database::saveDocument(MutableDocument &doc) {
         (void) saveDocument(doc, kCBLConcurrencyControlLastWriteWins);
     }
-
 
     inline bool Database::saveDocument(MutableDocument &doc, CBLConcurrencyControl c) {
         CBLError error;
@@ -208,7 +251,6 @@ namespace cbl {
             CBLDatabase_SaveDocumentWithConcurrencyControl(ref(), doc.ref(), c, &error),
             error);
     }
-
 
     inline bool Database::saveDocument(MutableDocument &doc,
                                        ConflictHandler conflictHandler)
@@ -239,8 +281,6 @@ namespace cbl {
         CBLError error;
         check(CBLDatabase_PurgeDocument(ref(), doc.ref(), &error), error);
     }
-
-
 }
 
 CBL_ASSUME_NONNULL_END
