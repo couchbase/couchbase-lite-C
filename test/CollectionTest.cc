f1b0b11d7a0bb15e6b231989b416949818a12522
Pasin Suriyentrakorn <pasin@couchbase.com>
2022-10-25 17:00:43 -0700

    Fix per code review

--- test/CollectionTest.cc
+++ test/CollectionTest.cc
@@ -24,14 +24,16 @@
 using namespace fleece;
 using namespace std;
 
+static const size_t kDocIDBufferSize = 20;
+
 class CollectionTest : public CBLTest {
     
 public:
     
     void createNumberedDocs(CBLCollection *col, unsigned n, unsigned start = 1) {
         for (unsigned i = 0; i < n; i++) {
-            char docID[10];
-            snprintf(docID, 10, "doc-%03u", start + i);
+            char docID[kDocIDBufferSize];
+            snprintf(docID, kDocIDBufferSize, "doc-%03u", start + i);
             auto doc = CBLDocument_CreateWithID(slice(docID));
             
             MutableDict props = CBLDocument_MutableProperties(doc);
