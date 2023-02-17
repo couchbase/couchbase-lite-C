16a7b7a2ae520e2e40f98ee7139c76f57d170c7b
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-02-16 19:25:39 -0800

    Update doxygen API docs (#425)
    * Update doxygen API docs
    
    * Fixed doc comment in CBLDatabase_DefaultCollection.
    * Run doxygen for helium
    * Rebase with master and run doxygen again

--- docs/C/html/search/all_d.js
+++ docs/C/html/search/all_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['networkinterface_0',['networkInterface',['../struct_c_b_l_replicator_configuration.html#a177f0439dbf5462d812fbd6e7881e8b9',1,'CBLReplicatorConfiguration']]],
-  ['nonnull_1',['NONNULL',['../_compiler_support_8h.html#ada64c64b4206846e8f545c2fbfb3f307',1,'CompilerSupport.h']]],
-  ['numdocs_2',['numDocs',['../struct_c_b_l_collection_change.html#a48ad496a44617738beb6aa5aad616436',1,'CBLCollectionChange']]]
+  ['nonnull_0',['NONNULL',['../_compiler_support_8h.html#ada64c64b4206846e8f545c2fbfb3f307',1,'CompilerSupport.h']]],
+  ['numdocs_1',['numDocs',['../struct_c_b_l_collection_change.html#a48ad496a44617738beb6aa5aad616436',1,'CBLCollectionChange']]]
 ];
--- docs/C/html/search/groups_1.js
+++ docs/C/html/search/groups_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['collection_0',['Collection',['../group__collection.html',1,'']]]
+  ['collection_0',['Collection',['../group__collection.html',1,'']]],
+  ['constants_1',['Constants',['../group__constants.html',1,'']]]
 ];
