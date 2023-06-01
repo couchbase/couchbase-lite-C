b8f9fd997fa11d8c781ef74c844cc993efa42c1a
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-06-01 10:33:55 -0700

    CBL-4573 : Generate the API docs
    We still keep the API doc in the repo, and as we have some changes to the API doc, we need to regenerate the doc.
    
    Note: We have CBL-4338 ticket to move it out so that the API doc will be generated during build instead.

--- docs/C/html/search/all_1.js
+++ docs/C/html/search/all_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['activity_0',['activity',['../struct_c_b_l_replicator_status.html#a96d2defe6b7bdef71834758920eb9e75',1,'CBLReplicatorStatus']]],
-  ['algorithm_1',['algorithm',['../struct_c_b_l_encryption_key.html#abd26baf9f10aa8ee44e69d1620f2d840',1,'CBLEncryptionKey']]],
-  ['authenticator_2',['authenticator',['../struct_c_b_l_replicator_configuration.html#aa2cd20835cc4e6b516510d441d419d60',1,'CBLReplicatorConfiguration']]]
+  ['acceptparentdomaincookies_0',['acceptParentDomainCookies',['../struct_c_b_l_replicator_configuration.html#a5cebe3c2e7e4ce41d875ce4bc4de1e75',1,'CBLReplicatorConfiguration']]],
+  ['activity_1',['activity',['../struct_c_b_l_replicator_status.html#a96d2defe6b7bdef71834758920eb9e75',1,'CBLReplicatorStatus']]],
+  ['algorithm_2',['algorithm',['../struct_c_b_l_encryption_key.html#abd26baf9f10aa8ee44e69d1620f2d840',1,'CBLEncryptionKey']]],
+  ['authenticator_3',['authenticator',['../struct_c_b_l_replicator_configuration.html#aa2cd20835cc4e6b516510d441d419d60',1,'CBLReplicatorConfiguration']]]
 ];
