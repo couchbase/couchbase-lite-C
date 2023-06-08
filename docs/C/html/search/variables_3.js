f6b3377eaedac86cb650ccc545c1e0e1b7025588
Pasin Suriyentrakorn <pasin@couchbase.com>
2023-06-08 14:01:24 -0700

    CBL-4574 : Regenerate API Doc

--- docs/C/html/search/variables_3.js
+++ docs/C/html/search/variables_3.js
@@ -7,7 +7,7 @@ var searchData=
   ['docids_4',['docIDs',['../struct_c_b_l_collection_change.html#a20349d9b7a062d89d2b732341194088c',1,'CBLCollectionChange']]],
   ['documentcount_5',['documentCount',['../struct_c_b_l_replicator_progress.html#a1de6365276a041880b0a85298c4bd4d8',1,'CBLReplicatorProgress']]],
   ['documentids_6',['documentIDs',['../struct_c_b_l_replication_collection.html#a9210788a4151363f309aac9182e06cc7',1,'CBLReplicationCollection::documentIDs()'],['../struct_c_b_l_replicator_configuration.html#a56dafb12033c1eb11c396fdceaf19a1d',1,'CBLReplicatorConfiguration::documentIDs()']]],
-  ['documentpropertydecryptor_7',['documentPropertyDecryptor',['../struct_c_b_l_replicator_configuration.html#a06df53035a02fff7fcc1f7081b0d9ac4',1,'CBLReplicatorConfiguration']]],
-  ['documentpropertyencryptor_8',['documentPropertyEncryptor',['../struct_c_b_l_replicator_configuration.html#a880ba9c3c8411c355584ee075569c92a',1,'CBLReplicatorConfiguration']]],
+  ['documentpropertydecryptor_7',['documentPropertyDecryptor',['../struct_c_b_l_replicator_configuration.html#ab49bef38ece8706b0ed57229a6c0c1b8',1,'CBLReplicatorConfiguration']]],
+  ['documentpropertyencryptor_8',['documentPropertyEncryptor',['../struct_c_b_l_replicator_configuration.html#aac0b7e61604aa4c241c5a53f4dc5dcc0',1,'CBLReplicatorConfiguration']]],
   ['domain_9',['domain',['../struct_c_b_l_error.html#a3d786296f00e0951eab5d728638ba5f1',1,'CBLError']]]
 ];
