7c20f166760d27aa5b6ef9a1134de2a74797f14e
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-06-12 21:03:58 -0700

    CBL-5851 : Change prediction callback signature to return mutable dictionary
    Changed prediction callback signature to return FLMutableDict instead of FLSliceResult to be inline with the other platforms. The returned FLMutableDict will be released after the callback is called.

--- include/cbl/CBLPrediction.h
+++ include/cbl/CBLPrediction.h
@@ -28,18 +28,18 @@ typedef struct {
     /** A pointer to any external data needed by the `prediction` callback, which will receive this as its first parameter. */
     void* _cbl_nullable context;
     
-    /** Called from within a query (or document indexing) to run the prediction.
+    /** Prediction callback, called from within a query (or document indexing) to run the prediction.
         @param context  The value of the CBLPredictiveModel's `context` field.
         @param input  The input dictionary from the query.
-        @return The output of the prediction function, encoded as a Fleece dictionary, or null if there is no output.
-        @note A null \ref FLSliceResult can be created by calling FLSliceResult_CreateWith(nullptr, 0).
+        @return The output of the prediction function as an FLMutableDict, or NULL if there is no output.
+        @note The output FLMutableDict will be automatically released after the prediction callback is called.
         @warning This function must be "pure": given the same input parameters it must always
-                produce the same output (otherwise indexes or queries may be messed up).
-                It MUST NOT alter the database or any documents, nor run a query: either of
-                those are very likely to cause a crash. */
-    FLSliceResult (* _cbl_nonnull prediction)(void* _cbl_nullable context, FLDict input);
+                 produce the same output (otherwise indexes or queries may be messed up).
+                 It MUST NOT alter the database or any documents, nor run a query: either of
+                 those are very likely to cause a crash. */
+    FLMutableDict _cbl_nullable (* _cbl_nonnull prediction)(void* _cbl_nullable context, FLDict input);
 
-    /** Called if the model is unregistered, so it can release resources. */
+    /** Unregistered callback, called if the model is unregistered, so it can release resources. */
     void (*_cbl_nullable unregistered)(void* context);
 } CBLPredictiveModel;
 
