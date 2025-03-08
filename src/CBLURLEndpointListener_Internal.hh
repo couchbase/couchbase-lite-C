14bf43e475e3a1910ca878b96bdd0fe0286d1437
Jianmin Zhao <jianmin.zhao@couchbase.com>
2025-03-07 17:47:36 -0800

    review feedback

--- src/CBLURLEndpointListener_Internal.hh
+++ src/CBLURLEndpointListener_Internal.hh
@@ -76,18 +76,10 @@ public:
         if (_c4listener) return true;
 
         bool succ = true;
-        slice dbname;
         DEFER {
-            if (!succ) {
-                for (C4Collection* coll: _collections) {
-                    // These collections have been successfully shared.
-                    (void)_c4listener->unshareCollection(dbname, coll);
-                }
-                (void)_c4listener->unshareDB(_db);
+            if (!succ && _c4listener) {
                 delete _c4listener;
                 _c4listener = nullptr;
-                _collections.clear();
-                _db = nullptr;
             }
         };
 
@@ -104,45 +96,33 @@ public:
 
         _c4listener = new C4Listener(c4config);
 
-        CBLDatabase* cblDb = CBLCollection_Database(_conf.collections[0]);
+        CBLDatabase* cblDb = _conf.collections[0]->database();
         cblDb->c4db()->useLocked([&](C4Database* db) {
-            dbname = db->getName();
-            if (_c4listener->shareDB(dbname, db)) {
-                _db = db;
-            }
-            if (_db) {
+            slice dbname = db->getName();
+            if ( (succ = _c4listener->shareDB(dbname, db)) ) {
                 for (unsigned i = 0; i < _conf.collectionCount; ++i) {
                     _conf.collections[i]->useLocked([&](C4Collection* coll) {
-                        if (_c4listener->shareCollection(dbname, coll)) {
-                            _collections.push_back(coll);
-                        }
+                        succ = _c4listener->shareCollection(dbname, coll);
                     });
-                    if (_collections.size() < i + 1) break;
+                    if (!succ) break;
                 }
             }
         });
-        return (succ = _collections.size() == _conf.collectionCount);
+
+        return succ;
     }
 
     void stop() {
-        if (!_c4listener) return;
-
-        auto dbname = _db->getName();
-        for (C4Collection* coll: _collections) {
-            (void)_c4listener->unshareCollection(dbname, coll);
+        if (_c4listener) {
+            delete _c4listener;
+            _c4listener = nullptr;
         }
-        (void)_c4listener->unshareDB(_db);
-
-        c4listener_free(_c4listener);
-        _c4listener = nullptr;
     }
 
 private:
     const CBLURLEndpointListenerConfiguration _conf;
     mutable uint16_t                          _port{0};
     C4Listener* _cbl_nullable                 _c4listener{nullptr};
-    C4Database* _cbl_nullable                 _db;
-    std::vector<C4Collection*>                _collections;
 };
 
 CBL_ASSUME_NONNULL_END
