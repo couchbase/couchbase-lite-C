e0c0c20dd60b1abb1f6b71f2d69d736604e20dbd
Pasin <pasin@couchbase.com>
2026-03-05 15:37:42 -0800

    CBL-7926 : Include C++ headers in iOS Framework
    * Made CBL C++ headers avaiable as the public headers.
    
    * Updated the scripts that copy Fleece headers to include C++ headers and filtered out the expert headers.
    
    * Extracted the scripts that copy and rewrite include paths into their own script files.

---
+++ scripts/rewrite_framework_header_includes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Rewrites quoted #include directives in the framework's public headers
+# to use framework-style angle-bracket includes.
+# e.g., #include "fleece/FLSlice.h" → #include <CouchbaseLite/FLSlice.h>
+
+set -e
+
+pushd "$BUILT_PRODUCTS_DIR/$PUBLIC_HEADERS_FOLDER_PATH/"
+
+sed -i '' -E 's/#include "([^"]*\/)?([^"]+)"/#include <CouchbaseLite\/\2>/' *
